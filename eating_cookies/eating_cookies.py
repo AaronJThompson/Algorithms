5b3db189cfeaad0a8bfb8e3c2bcea26d4bcb5dae
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-10 14:17:40 +0100

    Add caching for optimization

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,9 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookies(n, cache=dict()):
+  if n in cache:
+    return cache[n]
   if n <= 0:
     return 1
   total = 0
@@ -14,12 +16,15 @@ def eating_cookies(n, cache=None):
       total += eating_cookies(n - i)
     elif i == n:
       total += 1
+  cache[n] = total
   return total
     
 
-if __name__ == "__main__":
+""" if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    print('Usage: eating_cookies.py [num_cookies]') """
+
+eating_cookies(50)
